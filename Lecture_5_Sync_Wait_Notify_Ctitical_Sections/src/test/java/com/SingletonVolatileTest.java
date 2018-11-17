bc5d9b9868df44a1869bc8439494d6696a6c93a0
Stern <sophiek172@gmail.com>
2018-11-17 15:10:14 +0300

    Krylova lec5 tests fixed

--- Lecture_5_Sync_Wait_Notify_Ctitical_Sections/src/test/java/com/SingletonVolatileTest.java
+++ Lecture_5_Sync_Wait_Notify_Ctitical_Sections/src/test/java/com/SingletonVolatileTest.java
@@ -1,68 +1,72 @@
 package com;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
 
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.IntStream;
-
-import static org.junit.Assert.assertEquals;
+import org.junit.Test;
 
 public class SingletonVolatileTest {
 
-    private static class SingletonVolatile {
-        private AtomicInteger counter = new AtomicInteger();
-        private static /*TODO: fix here*/ SingletonVolatile instance = null;
+  @Test
+  public void testSingleton() throws InterruptedException {
 
-        private SingletonVolatile() {
-        }
+    for (int i = 0; i < 10; i++) {
+      SingletonVolatile.getInstance().setCounter(0);
 
-        public void inc() {
-            counter.incrementAndGet();
-        }
+      new Thread(() -> {
+        IntStream.range(0, 1000).forEach((x) -> {
+          SingletonVolatile.getInstance().inc();
+        });
+      }).start();
 
-        public static SingletonVolatile getInstance() {
-            //TODO: Fix it here
-            if (instance == null) {
-                try {
-                    Thread.sleep(100); // keep sleep()
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-                instance = new SingletonVolatile();
-            }
-            return instance;
-        }
+      new Thread(() -> {
+        IntStream.range(0, 1000).forEach((x) -> {
+          SingletonVolatile.getInstance().inc();
+        });
+      }).start();
 
-        public int getCounter() {
-            return counter.get();
-        }
+      Thread.sleep(500);
 
-        public void setCounter(int i) {
-            counter.set(i);
-        }
+      assertEquals(2 * 1000, SingletonVolatile.getInstance().getCounter());
+    }
+  }
+
+  private static class SingletonVolatile {
+
+    private static final Object lock = new Object();
+    private static /*TODO: fix here*/ volatile SingletonVolatile instance = null;
+    private AtomicInteger counter = new AtomicInteger();
+
+    private SingletonVolatile() {
     }
 
-    @Test
-    public void testSingleton() throws InterruptedException {
-
-        for (int i = 0; i < 10; i++) {
-            SingletonVolatile.getInstance().setCounter(0);
-
-            new Thread(() -> {
-                IntStream.range(0, 1000).forEach((x) -> {
-                    SingletonVolatile.getInstance().inc();
-                });
-            }).start();
-
-            new Thread(() -> {
-                IntStream.range(0, 1000).forEach((x) -> {
-                    SingletonVolatile.getInstance().inc();
-                });
-            }).start();
-
-            Thread.sleep(500);
-
-            assertEquals(2 * 1000, SingletonVolatile.getInstance().getCounter());
+    public static SingletonVolatile getInstance() {
+      //TODO: Fix it here
+      synchronized (lock) {
+        if (instance == null) {
+          try {
+            Thread.sleep(100); // keep sleep()
+          } catch (InterruptedException e) {
+            e.printStackTrace();
+          }
+          instance = new SingletonVolatile();
         }
+        return instance;
+
+      }
     }
+
+    public void inc() {
+      counter.incrementAndGet();
+    }
+
+    public int getCounter() {
+      return counter.get();
+    }
+
+    public void setCounter(int i) {
+      counter.set(i);
+    }
+  }
 }
