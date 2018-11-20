c76ade9046f16ea6ea1bf7c4cf5b97033f40231e
Sofia_Krylova <sofia_krylova@epam.com>
2018-11-20 16:01:14 +0300

    Krylova lec14

--- Lecture_14_Completable_Future_Concurrency_practice/src/test/java/com/ATestThreadState.java
+++ Lecture_14_Completable_Future_Concurrency_practice/src/test/java/com/ATestThreadState.java
@@ -1,51 +1,73 @@
 package com;
 
-import org.junit.Test;
-
+import static java.lang.Thread.sleep;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
 
 /**
  * Created by Admin on 8/6/2018.
  */
 public class ATestThreadState {
 
-    /**
-     * Fill in the gaps and insert instructions to make code executable
-     * @throws InterruptedException
-     */
-    @Test
-    public void testThreadState() throws InterruptedException {
-        // TODO: change instantiation
-        Thread thread1 = null;
-        Thread thread2 = null;
+  /**
+   * Fill in the gaps and insert instructions to make code executable
+   */
+  @Test
+  public void testThreadState() throws InterruptedException {
+    // TODO: change instantiation
+    Runnable task = () -> {
+      for (int i =0; i < 10; i++) {
+        System.out.println("task " + i);
+        try {
+          sleep(1000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    };
+    Runnable task1 = () -> {
+      for (int i =0; i < 10; i++) {
+        System.out.println("task " + i);
+        try {
+          sleep(2000);
+        } catch (InterruptedException e) {
+          e.printStackTrace();
+        }
+      }
+    };
+    Thread thread1 = createThread(task);
+    Thread thread2 = createThread(task1);
 
-        assertEquals(thread1.getState(), Thread.State.NEW);
-        assertEquals(thread2.getState(), Thread.State.NEW);
+    assertEquals(thread1.getState(), Thread.State.NEW);
+    assertEquals(thread2.getState(), Thread.State.NEW);
 
+    // TODO: fill the gap
+    // TODO: fill the gap
+    thread1.start();
+    thread2.start();
 
-        // TODO: fill the gap
-        // TODO: fill the gap
+    assertEquals(thread1.getState(), Thread.State.RUNNABLE);
+    assertEquals(thread2.getState(), Thread.State.RUNNABLE);
 
-        assertEquals(thread1.getState(), Thread.State.RUNNABLE);
-        assertEquals(thread2.getState(), Thread.State.RUNNABLE);
+    // Add delay if necessary
+    // TODO: fill the gap
 
-        // Add delay if necessary
-        // TODO: fill the gap
+    thread2.join(5000);
+    // threads should run task to be put on hold
+    assertEquals(thread1.getState(), Thread.State.TIMED_WAITING);
+    assertEquals(thread2.getState(), Thread.State.TIMED_WAITING);
+    assertEquals(Thread.currentThread().getState(), Thread.State.RUNNABLE);
+  }
 
-        // threads should run task to be put on hold
-        assertEquals(thread1.getState(), Thread.State.TIMED_WAITING);
-        assertEquals(thread2.getState(), Thread.State.TIMED_WAITING);
-        assertEquals(Thread.currentThread().getState(), Thread.State.RUNNABLE);
-    }
+  private Thread createThread() {
+    final Thread thread = new Thread();
+    return thread;
+  }
 
-    private Thread createThread() {
-        final Thread thread = new Thread();
-        return thread;
-    }
-
-    private Thread createThread(Runnable runnable) {
-        final Thread thread = new Thread(runnable);
-        return thread;
-    }
+  private Thread createThread(Runnable runnable) {
+    final Thread thread = new Thread(runnable);
+    return thread;
+  }
 
 }
